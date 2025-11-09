01d5f83078298b3dd39791788b2f4aded6824695
LNK <lekoev.nvk@gmail.com>
2025-11-09 23:26:45 +0500

    Completed task

--- Test_task_1.py
+++ Test_task_1.py
@@ -39,7 +39,7 @@ catalog_product_1_name = driver.find_element(By.XPATH, "//a[@id='item_4_title_li
 catalog_product_1_name_value = catalog_product_1_name.text
 catalog_product_1_price = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[1]")
 catalog_product_1_price_value = catalog_product_1_price.text.replace('$', '')
-print(f"Passed. Name = {catalog_product_1_name_value}; Price = {catalog_product_1_price_value}$.\n")
+print(f"Passed\n")
 
 """---Product 2---"""
 print("Trying get catalog product 2 name and price")
@@ -47,7 +47,7 @@ catalog_product_2_name = driver.find_element(By.XPATH, "//a[@id='item_0_title_li
 catalog_product_2_name_value = catalog_product_2_name.text
 catalog_product_2_price = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[2]")
 catalog_product_2_price_value = catalog_product_2_price.text.replace('$', '')
-print(f"Passed. Name = {catalog_product_2_name_value}; Price = {catalog_product_2_price_value}$.\n")
+print(f"Passed\n")
 
 """--------------Get "Add to cart" and Cart Buttons of Products From Catalog--------------------"""
 add_to_cart_product_1 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
@@ -89,6 +89,48 @@ assert cart_product_2_name_value == catalog_product_2_name_value
 assert cart_product_2_price_value == catalog_product_2_price_value
 print("Passed\n")
 
+"""--------------Open Checkout--------------------"""
+print("Trying to find checkout button and press it")
+checkout_button = driver.find_element(By.XPATH, "//button[@id='checkout']")
+checkout_button.click()
+print("Passed\n")
+
+"""--------------Fill Checkout Form And Press Continue--------------------"""
+print("Trying to complete checkout form")
+driver.find_element(By.XPATH, "//input[@id='first-name']").send_keys("Test")
+driver.find_element(By.XPATH, "//input[@id='last-name']").send_keys("Testovich")
+driver.find_element(By.XPATH, "//input[@id='postal-code']").send_keys("1234567")
+driver.find_element(By.XPATH, "//input[@id='continue']").click()
+print("Passed\n")
+
+"""--------------Get Products From Checkout And Compare With Cart--------------------"""
+"""---Product 1---"""
+print("Trying get checkout product 1 data and compare with cart")
+checkout_product_1_name = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
+checkout_product_1_name_value = checkout_product_1_name.text
+checkout_product_1_price = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[1]")
+checkout_product_1_price_value = checkout_product_1_price.text.replace('$', '')
+assert checkout_product_1_name_value == checkout_product_1_name_value
+assert checkout_product_1_price_value == checkout_product_1_price_value
+print("Passed\n")
+
+"""---Product 2---"""
+print("Trying get checkout product 1 data and compare with cart")
+checkout_product_2_name = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
+checkout_product_2_name_value = checkout_product_2_name.text
+checkout_product_2_price = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[2]")
+checkout_product_2_price_value = checkout_product_2_price.text.replace('$', '')
+assert checkout_product_2_name_value == checkout_product_2_name_value
+assert checkout_product_2_price_value == checkout_product_2_price_value
+print("Passed\n")
+
+"""--------------Check the correctness of the price--------------------"""
+print("Trying to get checkout and item_total prices and compare them")
+checkout_total_price = float(checkout_product_1_price_value) + float(checkout_product_2_price_value)
+item_total = driver.find_element(By.XPATH, "//div[@class='summary_subtotal_label']").text.replace('$', '')
+item_total_price = float(''.join(c for c in item_total if c.isdigit() or c == '.'))
+assert checkout_total_price == item_total_price
+print("Passed\n")
 
 """--------------Sleep and close browser--------------------"""
 
